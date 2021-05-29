45f578fc838900d45a50d385710dc072da3cf005
pshadaram@gmail.com <pshadaram@gmail.com>
2021-05-29 14:46:02 -0500

    filter

--- script.js
+++ script.js
@@ -91,3 +91,8 @@ const id = setInterval(() => {
 
 clearInterval(id)
 
+/* filter */
+const userNames = ["mark","staceymom2978","lajsdflj","carrie98"];
+
+const validUserNames = userNames.filter(x => 
+    x.length() < 10);
