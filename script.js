c7b5f85ed32b697098e9d5e6a6aae74b404f27b4
“[Page <page.shadaram@gmail.com>
2021-06-01 20:18:50 -0500

    commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 const userNames = ["mark","staceymom2978","lajsdflj","carrie98"];
 
-const validUserNames = userNames.filter(x => 
-    x.length() < 10);
-
+const validUserNames = userNames.filter(x => x.length < 10);
